99e33e572a39b90be32dc2b05bb8eb7c0d3e6fc8
elghaddourimohamed1 <elghaddourimohamed1@gmail.com>
2026-06-20 08:47:46 +0200

    email validation

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+let users = [];
+
+
+
+
+
+//* If the user chooses to sign up
+
+// // Email
+// function email(email) {
+// let checkForEmailSpaces = email.replace(/\s/g, "").toLowerCase();
+// if (checkForEmailSpaces.length > 10 && checkForEmailSpaces.includes('@')) {
+//     return checkForEmailSpaces;
+// }else{
+//     alert("Your email address must contain at least 10 characters and an @ symbol."); 
+// }
+// }
+// let finalValidEmail = null;
+
+
+
+
+
+
+
+
